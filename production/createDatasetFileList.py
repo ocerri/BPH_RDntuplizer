84dd7dc7891a27d17b9d1c7de10fabbd1da82d73
Olmo Cerri <olmo.cerri@cern.ch>
2019-11-05 12:01:07 -0800

    private MC samples

--- production/createDatasetFileList.py
+++ production/createDatasetFileList.py
@@ -6,6 +6,7 @@ prod_samples = yaml.load(open('samples.yml'))
 
 for k, d in prod_samples['samples'].iteritems():
     if 'data_' in k:
+        continue
         for i in d['parts']:
             dataset = d['dataset'].format(i)
             print '########## {} ##########\n'.format(dataset)
@@ -13,3 +14,14 @@ for k, d in prod_samples['samples'].iteritems():
             cmd = 'das_client --query="file dataset={}" --limit=0 > {}.txt'.format(dataset, fname)
             os.system(cmd)
             print '\n'
+    else:
+        print '########## {} ##########\n'.format(k)
+        dataset = d['dataset']
+        fname = 'inputFiles' + dataset.replace('/', '_')
+        cmd = 'das_client --query="file dataset={}'.format(dataset)
+        if dataset[-4:] == 'USER':
+            cmd += ' instance=prod/phys03'
+        cmd += '"'
+        cmd += ' --limit=0 > {}.txt'.format(fname)
+        os.system(cmd)
+        print '\n'
